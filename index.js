bd03bbc2086e9cb57bcdd9f34553b17393b9d309
whisnu panji <you@example.com>
2021-06-02 19:50:52 +0700

    update file

---
+++ index.js
@@ -0,0 +1,17 @@
+// Button of Kontak Saya
+function show(obj) {
+    obj.innerHTML = "Wpanji8@gmail.com";
+}
+
+function hide(obj) {
+    obj.innerHTML = "E-mail";
+}
+
+function show2(obj) {
+    obj.innerHTML = "@callme_junot";
+}
+
+function hide2(obj) {
+    obj.innerHTML = "Instagram";
+}
+
